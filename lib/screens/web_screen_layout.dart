69f9ede9b41651c48f0acabb16bfc711989f1a17
JabidHasan <jabid9831@yahoo.com>
2022-09-28 16:12:15 +0600

    Web contact list completed

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_whatsapp_ui/colors.dart';
 import 'package:flutter_whatsapp_ui/widgets/contact_list.dart';
+import 'package:flutter_whatsapp_ui/widgets/web_profile_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
@@ -15,6 +17,7 @@ class WebScreenLayout extends StatelessWidget {
               child: Column(
                 children: [
                   // Web Profile Bar
+                  WebProfileBar(),
                   // Web Search Bar
                   ContactsList(),
                 ],
