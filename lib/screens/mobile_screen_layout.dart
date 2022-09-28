d4390ef8240466ca59af08b6b5d12adf1bfeeb2c
JabidHasan <jabid9831@yahoo.com>
2022-09-28 14:48:49 +0600

    Mobile contact list completed

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,66 +1,6 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_whatsapp_ui/colors.dart';
-
-// class MobileScreenLayout extends StatelessWidget {
-//   const MobileScreenLayout({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return DefaultTabController(
-//       length: 3,
-//       child: Scaffold(
-//         appBar: AppBar(
-//           backgroundColor: appBarColor,
-//           centerTitle: false,
-//           title: const Text(
-//             "WhatsApp",
-//             style: TextStyle(
-//               color: Colors.grey,
-//               fontSize: 20,
-//               fontWeight: FontWeight.bold,
-//             ),
-//           ),
-//           actions: [
-//             IconButton(
-//                 onPressed: () {},
-//                 icon: const Icon(
-//                   Icons.search,
-//                   color: Colors.grey,
-//                 )),
-//             IconButton(
-//                 onPressed: () {},
-//                 icon: const Icon(
-//                   Icons.more_vert,
-//                   color: Colors.grey,
-//                 )),
-//           ],
-//           bottom: const TabBar(
-//             indicatorColor: tabColor,
-//             indicatorWeight: 4,
-//             labelColor: tabColor,
-//             unselectedLabelColor: Colors.grey,
-//             labelStyle: TextStyle(fontWeight: FontWeight.bold),
-//             tabs: [
-//               Tab(
-//                 text: "CHATS",
-//               ),
-//               Tab(
-//                 text: "STATUS",
-//               ),
-//               Tab(
-//                 text: "CALLS",
-//               ),
-//             ],
-//           ),
-//         ),
-//         body: const Center(child: Text("Mobile")),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_whatsapp_ui/colors.dart';
+import 'package:flutter_whatsapp_ui/widgets/contact_list.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
@@ -71,6 +11,7 @@ class MobileScreenLayout extends StatelessWidget {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
+          elevation: 0,
           backgroundColor: appBarColor,
           centerTitle: false,
           title: const Text(
@@ -116,9 +57,7 @@ class MobileScreenLayout extends StatelessWidget {
             ],
           ),
         ),
-        body: const Center(
-          child: Text("Moblie"),
-        ),
+        body: const ContactsList(),
       ),
     );
   }
