a84f1108f3c30b762d953a88b6f084ef0f65291d
JabidHasan <jabid9831@yahoo.com>
2022-09-29 20:32:23 +0600

    Mobile chat screen

---
+++ lib/screens/mobile_chat_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_whatsapp_ui/colors.dart';
+import 'package:flutter_whatsapp_ui/info.dart';
+import 'package:flutter_whatsapp_ui/widgets/chat_list.dart';
+
+class MobileChatScreen extends StatelessWidget {
+  const MobileChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: appBarColor,
+        title: Text(
+          info[0]['name'].toString(),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.video_call)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+        ],
+      ),
+      body: Column(
+        children: [
+          // Chat List
+          const Expanded(child: ChatList()),
+          TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: mobileChatBoxColor,
+              prefix: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Icon(
+                  Icons.emoji_emotions,
+                  color: Colors.grey,
+                ),
+              ),
+              suffixIcon: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    Icon(
+                      Icons.camera_alt,
+                      color: Colors.grey,
+                    ),
+                    Icon(
+                      Icons.attach_file,
+                      color: Colors.grey,
+                    ),
+                    Icon(
+                      Icons.money,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+              ),
+              hintText: 'Type a message',
+              contentPadding: const EdgeInsets.all(10),
+            ),
+          )
+          // Text Input for message
+        ],
+      ),
+    );
+  }
+}
